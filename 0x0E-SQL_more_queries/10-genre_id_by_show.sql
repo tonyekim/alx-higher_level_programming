ca1f4356cb1a7151772f56a5cfdc6f8f2d0083db
tonyekim <otoobongekim@gmail.com>
2022-11-02 13:14:36 -0700

    GENRE ID by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -8,8 +8,8 @@
 -- can use only one SELECT statement
 -- the database name will be passed as an argument of the mysql command
 
-SELECT tv_shows.title, tv_show_genre.genre.id
-FROM tv_shows
+    SELECT tv_shows.title, tv_show_genres.genre_id
+      FROM tv_shows
 INNER JOIN tv_show_genres
-ON tv_shows.id = tv_shows_genre.show.id
-ORDER BY tv_shows.title, tv_show_genre.genre_id ASC;
+        ON tv_shows.id = tv_show_genres.show_id
+  ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
