3fa56f010535c7daa5f99924062ef25c780edb5c
tonyekim <otoobongekim@gmail.com>
2022-11-02 12:57:29 -0700

    cities TABLE

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,9 +6,10 @@
 -- should not fail if either exists
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	state_id INT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
+       id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+       state_id INT NOT NULL,
+       name VARCHAR(256) NOT NULL,
+       FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
 );
