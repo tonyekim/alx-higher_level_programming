38faacce189c0e220bda10294cdc7bf84037930e
tonyekim <otoobongekim@gmail.com>
2022-11-29 13:19:12 -0800

    updating files

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-
-const dict = require('./101-data').dict;
-
-let newDic = {};
+const dict = require('./101-data.js').dict;
+let newDict = {};
 for (let key in dict) {
-  if (newDic[dict[key]] === undefined) {
-    newDic[dict[key]] = [];
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
   }
-  newDic[dict[key]].push(key);
 }
-
-console.log(newDic);
+console.log(newDict);
