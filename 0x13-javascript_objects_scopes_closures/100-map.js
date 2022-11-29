27ada0fae40d176279f875ce5d3a0a42fabcda54
tonyekim <otoobongekim@gmail.com>
2022-11-29 13:16:51 -0800

    updating files

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-
-const newList = list.map((x, index) => x * index);
-
+const list = require('./100-data.js').list;
 console.log(list);
-console.log(newList);
+console.log(list.map((item, index) => item * index));
