e91c2b406b34309fcf6fafc07ffb838b9fd68b39
ANTHONY EKIM <97892127+tonyekim@users.noreply.github.com>
2023-01-17 13:06:54 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-let filename = process.argv[2];
 const fs = require('fs');
-fs.readFile(filename, 'utf8', function (err, data) {
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
-    console.log(data);
+    process.stdout.write(data);
   }
 });
