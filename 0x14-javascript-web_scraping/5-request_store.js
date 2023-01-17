664dea61c7d681ea685e2275d0093a597c82eeaf
ANTHONY EKIM <97892127+tonyekim@users.noreply.github.com>
2023-01-17 13:13:51 +0100

    Gets the contents of a webpage and stores it in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 
-let url = process.argv[2];
-let filename = process.argv[3];
-const fs = require('fs');
 const request = require('request');
+const fs = require('fs');
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(filename, body, 'utf8');
-  }
+request(process.argv[2], function (_err, _res, body) {
+  fs.writeFile(process.argv[3], body, 'utf8', function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
 });
